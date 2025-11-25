92a85cc4cb7cc30d2a0f7208e8b693e0619b32ce
Amelea <amelea.nordfors@yh.nackademin.se>
2025-11-25 11:34:56 +0100

    had to copy/paste Johans entire file + changed name of folder

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,7 +1,7 @@
 import { type ClientSchema, a, defineData, defineFunction } from "@aws-amplify/backend";
 
-  const iotCoreHandler = defineFunction({
-  entry: '/iotCoreHandler/handler.ts'
+const iotCoreHandler = defineFunction({
+  entry: './deviceHandler/handler.ts'
 })
 
 /*== STEP 1 ===============================================================
@@ -11,16 +11,12 @@ specifies that any user authenticated via an API key can "create", "read",
 "update", and "delete" any "Todo" records.
 =========================================================================*/
 const schema = a.schema({
-  Todo: a
-  .model({
-    content: a.string(),
-  })
-  .authorization((allow) => [allow.owner()]),
+
   Devices: a
     .model({
       device_id: a.string().required(),
       owner: a.string().required(),
-      status: a.string(),
+      status: a.string()
     })
     .identifier(['device_id'])
     .authorization((allow) => [allow.owner(), allow.publicApiKey()]),
@@ -28,7 +24,7 @@ const schema = a.schema({
   Telemetry: a
     .model({
       device_id: a.string().required(),
-      timestamp: a.datetime().required(),
+      timestamp: a.timestamp().required(),
       temperature: a.float(),
       humidity: a.float(),
       owner: a.string().required()
@@ -36,28 +32,29 @@ const schema = a.schema({
     .identifier(['device_id', 'timestamp'])
     .authorization((allow) => [allow.owner(), allow.publicApiKey()]),
 
-
-  AddTelemetry: a
-  .mutation()
-  .arguments({
-    device_id: a.string().required(),
-    timestamp: a.timestamp().required(),
-    temperature: a.float(),
-    humidity: a.float(),
-    owner: a.string().required()
-  })
+    AddTelemetry: a
+    .mutation()
+    .arguments({
+      device_id: a.string().required(),
+      timestamp: a.timestamp().required(),
+      temperature: a.float(),
+      humidity: a.float(),
+      owner: a.string().required()
+    }
+    )
     .returns(a.ref("Telemetry"))
     .authorization((allow) => [allow.publicApiKey()])
     .handler(a.handler.function(iotCoreHandler)),
+});
 
-  });
+
 
 export type Schema = ClientSchema<typeof schema>;
 
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: 'userPool',
+    defaultAuthorizationMode: "userPool",
     apiKeyAuthorizationMode: {
       expiresInDays: 365,
     },
@@ -69,7 +66,7 @@ Go to your frontend source code. From your client-side code, generate a
 Data client to make CRUDL requests to your table. (THIS SNIPPET WILL ONLY
 WORK IN THE FRONTEND CODE FILE.)
 
-Using JavaScript or Next.js React Server Components, Middleware, Server 
+Using JavaScript or Next.js React Server Components, Middleware, Server
 Actions or Pages Router? Review how to generate Data clients for those use
 cases: https://docs.amplify.aws/gen2/build-a-backend/data/connect-to-API/
 =========================================================================*/
@@ -77,7 +74,7 @@ cases: https://docs.amplify.aws/gen2/build-a-backend/data/connect-to-API/
 /*
 "use client"
 import { generateClient } from "aws-amplify/data";
-import type { Schema } from "@/amplify/datgita/resource";
+import type { Schema } from "@/amplify/data/resource";
 
 const client = generateClient<Schema>() // use this Data client for CRUDL requests
 */
